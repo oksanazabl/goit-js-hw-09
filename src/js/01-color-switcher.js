a1c9c39907fbad0121ee9ab9406ec479ec739823
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-02-26 19:50:56 +0200

    add 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const backgroundColor = getRandomHexColor();
+    document.body.style.backgroundColor = `${backgroundColor}`;
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  if (timerId) {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+  }
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
